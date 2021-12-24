4d26cda54be4abb2184fab2c6da1dd0ce0de5c23
AlexusYT <alexus_xx@mail.ru>
2021-12-24 20:42:38 +0500

    Refactoring

--- src/main/java/ru/alexus/twitchbot/Utils.java
+++ src/main/java/ru/alexus/twitchbot/Utils.java
@@ -1,6 +1,6 @@
 package ru.alexus.twitchbot;
 
-import org.apache.commons.lang3.text.WordUtils;
+import org.apache.commons.text.WordUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import ru.alexus.twitchbot.langTypos.LangTypos_v2;
